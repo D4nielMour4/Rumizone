ad24559a0bb8cd5ede95f32446f0427a6b615da5
wendellvsilva <141570270+wendellvsilva@users.noreply.github.com>
2023-08-17 15:32:02 -0300

    codigo wendell.py

--- Code/wendel.py
+++ Code/wendel.py
@@ -7,51 +7,77 @@ customtkinter.set_default_color_theme("green")
 def clique():
     print("Fazer login")
 
+def voltar_para_inicio(frame_atual):
+    frame_atual.pack_forget()
+    frame_inicio.pack()
+
 def janela_login():
-    nova_janela = customtkinter.CTk()
-    nova_janela.geometry("800x550")
-    nova_janela.title("Sessão do Usuário")
-    texto = customtkinter.CTkLabel(nova_janela, text="Fazer login")
-    texto.pack(padx=10, pady=10)
-    email = customtkinter.CTkEntry(nova_janela, placeholder_text="Seu e-mail")
-    email.pack(padx=10, pady=10)
-    senha = customtkinter.CTkEntry(nova_janela, placeholder_text="Digite sua senha", show="*")
-    senha.pack(padx=10, pady=10)
-    checkbox = customtkinter.CTkCheckBox(nova_janela, text="Lembrar login")
-    checkbox.pack(padx=10, pady=10)
-    botao = customtkinter.CTkButton(nova_janela, text="Login", command=clique)
-    botao.pack(padx=10, pady=10)
-    nova_janela.mainloop()
-    
+    frame_inicio.pack_forget()
+    frame_registro.pack_forget() 
+    frame_login.pack()
+
 def janela_registro():
-    nova_janela2 = customtkinter.CTk()
-    nova_janela2.geometry("800x550")
-    nova_janela2.title("Sessão de registro")
-    
-    
+    frame_inicio.pack_forget()
+    frame_login.pack_forget()
+    frame_registro.pack()
 
 janela = customtkinter.CTk()
-janela.geometry("800x550")
+janela.geometry("900x650")
 janela.resizable(False, False)
 janela.title("Rumizone")
 
-espaco = customtkinter.CTkLabel(janela, text=" ")
+frame_inicio = Frame(janela)
+frame_inicio.pack()
+
+espaco = customtkinter.CTkLabel(frame_inicio, text=" ")
 espaco.pack(padx=10, pady=10)
 espaco.pack(padx=10, pady=10)
-espaco1 = customtkinter.CTkLabel(janela, text=" ")
+espaco1 = customtkinter.CTkLabel(frame_inicio, text=" ")
 espaco1.pack(padx=10, pady=10)
-espaco2 = customtkinter.CTkLabel(janela, text=" ")
+espaco2 = customtkinter.CTkLabel(frame_inicio, text=" ")
 espaco2.pack(padx=10, pady=10)
-texto = customtkinter.CTkLabel(janela, text="Rumizone", font=("Helvetica", 36))
+texto = customtkinter.CTkLabel(frame_inicio, text="Rumizone", text_color="#2ca474", font=("Helvetica", 36))
 texto.pack(padx=10, pady=10)
-texto1 = customtkinter.CTkLabel(janela, text="Monitoramento e comportamento animal", font=("Helvetica", 14))
+texto1 = customtkinter.CTkLabel(frame_inicio, text="Monitoramento e comportamento animal", font=("Helvetica", 14))
 texto1.pack(padx=5, pady=5)
 
-botao3 = customtkinter.CTkButton(janela, text="Login", command=janela_login)
+botao3 = customtkinter.CTkButton(frame_inicio, text="Login", command=janela_login)
 botao3.pack(padx=10, pady=10)
 
-botao2 = customtkinter.CTkButton(janela, text="Registre-se")
+botao2 = customtkinter.CTkButton(frame_inicio, text="Registre-se", command=janela_registro)
 botao2.pack(padx=10, pady=10)
 
+frame_login = Frame(janela)
+frame_registro = Frame(janela)
+
+texto_login = customtkinter.CTkLabel(frame_login, text="Fazer login")
+texto_login.pack(padx=10, pady=10)
+email = customtkinter.CTkEntry(frame_login, placeholder_text="Seu e-mail")
+email.pack(padx=10, pady=10)
+senha = customtkinter.CTkEntry(frame_login, placeholder_text="Digite sua senha", show="*")
+senha.pack(padx=10, pady=10)
+checkbox = customtkinter.CTkCheckBox(frame_login, text="Lembrar login")
+checkbox.pack(padx=10, pady=10)
+botao_login = customtkinter.CTkButton(frame_login, text="Login", command=clique)
+botao_login.pack(padx=10, pady=10)
+botao_voltar_login = customtkinter.CTkButton(frame_login, text="Voltar", command=lambda: voltar_para_inicio(frame_login))
+botao_voltar_login.pack(padx=10, pady=10)
+
+texto_registro = customtkinter.CTkLabel(frame_registro, text="Fazer registro")
+texto_registro.pack(padx=10, pady=10)
+email_registro = customtkinter.CTkEntry(frame_registro, placeholder_text="Digite seu e-mail")
+email_registro.pack(padx=10, pady=10)
+confirmacao_senha = customtkinter.CTkEntry(frame_registro, placeholder_text="Digite sua senha de novo", show="*")
+confirmacao_senha.pack(padx=10, pady=10)
+senha_registro = customtkinter.CTkEntry(frame_registro, placeholder_text="Digite sua senha", show="*")
+senha_registro.pack(padx=10, pady=10)
+checkbox_registro = customtkinter.CTkCheckBox(frame_registro, text="Lembrar registro")
+checkbox_registro.pack(padx=10, pady=10)
+botao_registro = customtkinter.CTkButton(frame_registro, text="Registrar", command=clique)
+botao_registro.pack(padx=10, pady=10)
+botao_voltar_registro = customtkinter.CTkButton(frame_registro, text="Voltar", command=lambda: voltar_para_inicio(frame_registro))
+botao_voltar_registro.pack(padx=10, pady=10)
 
 janela.mainloop()
+
+# Resolver problema da barra branca no meio de todas as telas
