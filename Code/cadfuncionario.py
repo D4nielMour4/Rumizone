f1b8a51f9abd80bacf85105101dc2a7933448fb6
wendellvsilva <wendellcrtss@gmail.com>
2023-08-22 21:02:18 -0300

    wendellatt
    atualizei algumas coisinhas básicas.

---
+++ Code/cadfuncionario.py
@@ -0,0 +1,56 @@
+import customtkinter
+from tkinter import *
+from PIL import Image, ImageTk
+from tkinter import filedialog
+from tkinter import messagebox
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+def escolher_imagem():
+    filepath = filedialog.askopenfilename(filetypes=[("Image files", "*.png *.jpg *.jpeg *.gif")])
+    if filepath:
+        imagem = Image.open(filepath)
+        imagem.thumbnail((292, 480))  # Redimensionar a imagem para o tamanho do ícone
+        imagem = ImageTk.PhotoImage(imagem)
+        label_imagem.config(image=imagem)
+        label_imagem.image = imagem
+
+telacadfunci = customtkinter.CTk(fg_color="#CFD8DC")
+telacadfunci.geometry("1360x720")
+telacadfunci.resizable(False, False)
+telacadfunci.title("Cadastro de funcionários")
+
+frame_inicio = customtkinter.CTkFrame(telacadfunci, fg_color="#CFD8DC")
+
+iconevaca1 = customtkinter.CTkImage(light_image=Image.open(r"C:\Users\wende\OneDrive\Documentos\GitHub\Rumizone\Code\iconevaquinha.png"),
+                                   dark_image=Image.open(r"C:\Users\wende\OneDrive\Documentos\GitHub\Rumizone\Code\iconevaquinha.png"), size=(292, 480))
+label_icone = customtkinter.CTkLabel(master=telacadfunci, text="", image=iconevaca1)
+label_icone.place(x=5, y=5)
+
+botao_escolher_imagem = customtkinter.CTkButton(telacadfunci, text="Escolher Imagem", command=escolher_imagem,
+                                                 fg_color="#8690AF", text_color="white")
+botao_escolher_imagem.place(x=20, y=570)
+
+label_imagem = Label(telacadfunci)
+label_imagem.place(x=5, y=5)
+
+nomefuncionario = customtkinter.CTkEntry(telacadfunci, placeholder_text="Digite seu nome",
+                                         text_color="black", fg_color="#FFDA8F", width=280, height=60, font=("Helvetica", 20))
+nomefuncionario.place(x=20, y=290)
+
+nascimentofuncionario = customtkinter.CTkEntry(telacadfunci, placeholder_text="Data de nasciment.",
+                                               text_color="black", fg_color="#8690AF", width=280, height=60, font=("Helvetica", 20))
+nascimentofuncionario.place(x=20, y=360)
+
+emailfuncionario = customtkinter.CTkEntry(telacadfunci, placeholder_text="Diga seu e-mail",
+                                           text_color="black", fg_color="#FF9EB1", width=280, height=60, font=("Helvetica", 20))
+emailfuncionario.place(x=20, y=430)
+
+telefonefuncionario = customtkinter.CTkEntry(telacadfunci, placeholder_text="Diga número de celular.",
+                                             text_color="black", fg_color="#8690AF", width=280, height=60, font=("Helvetica", 20))
+telefonefuncionario.place(x=20, y=500)
+
+
+
+telacadfunci.mainloop()
--- Code/wendel.py
+++ Code/wendel.py
@@ -1,9 +1,43 @@
 import customtkinter
 from tkinter import *
 from  PIL import Image
+from tkinter import filedialog 
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
+
+usuarios = {
+    "usuario1": "wendell",
+    "usuario2": "12345",
+    # apenas usuários testes
+}
+
+def verificar_login(login, senha):
+    if login in usuarios and usuarios[login] == senha:
+        return True
+    return False
+
+# Função que será chamada quando o botão de login for clicado
+def fazer_login():
+    login = email.get() 
+    senha = senha.get()  
+    
+    if verificar_login(login, senha):
+        print("Login bem-sucedido!")
+    else:
+        print("Login ou senha incorretos.")
+
+# Função que será chamada quando o botão de registro for clicado
+def criar_conta():
+    novo_usuario = email_registro.get()  
+    nova_senha = senha_registro.get()  
+    
+    if novo_usuario not in usuarios:
+        usuarios[novo_usuario] = nova_senha
+        print("Conta criada com sucesso!")
+    else:
+        print("Usuário já existe.")
+
 
 
 def clique():
@@ -29,38 +63,43 @@ tela.resizable(False, False)
 tela.title("Rumizone")  
 
 frame_inicio = customtkinter.CTkFrame(tela, fg_color="#CFD8DC")
-
 frame_inicio.pack()
 
 #Frame/Tela inicial
 
-iconevaca = customtkinter.CTkImage(light_image=Image.open(r"C:\Users\wende\OneDrive\Documentos\GitHub\Rumizone\Code\fodase.png"),
-                                   dark_image=Image.open(r"C:\Users\wende\OneDrive\Documentos\GitHub\Rumizone\Code\fodase.png"),
-                                   size=(250,320))
+iconevaca = customtkinter.CTkImage(light_image=Image.open(r"C:\Users\wende\OneDrive\Documentos\GitHub\Rumizone\Code\iconevaquinha.png"),
+                                   dark_image=Image.open(r"C:\Users\wende\OneDrive\Documentos\GitHub\Rumizone\Code\iconevaquinha.png"),
+                                   size=(292,480))
 Label = customtkinter.CTkLabel(master=tela, text="", image=iconevaca)
-Label.place(x=530, y=50)
+Label.place(x=514, y=5)
 texto = customtkinter.CTkLabel(tela, text="Rumizone", text_color="#607D8B", font=("Robot", 49))
-texto.place(x=560, y=250)
-texto1 = customtkinter.CTkLabel(tela, text="Monitoramento e comportamento animal", text_color="#607D8B", font=("Helvetica", 20))
-texto1.place(x=480, y=310)
-botao1 = customtkinter.CTkButton(tela, text="Login", font=("Helvetica",40), command=janela_login, fg_color="#8690AF",text_color="white", width=370, height=70)
+texto.place(x=560, y=290)
+#texto1 = customtkinter.CTkLabel(tela, text="Monitoramento e comportamento animal", text_color="#607D8B", font=("Helvetica", 20))
+#texto1.place(x=480, y=310)
+botao1 = customtkinter.CTkButton(tela, text="Login", font=("Helvetica",40),
+                                 command=janela_login, fg_color="#8690AF",text_color="white", width=370, height=70)
 botao1.place(x=480, y=350)
-botao2 = customtkinter.CTkButton(tela, text="Registre-se", font=("Helvetica",40), command=janela_registro, fg_color="#8690AF",text_color="white", width=370, height=70)
+botao2 = customtkinter.CTkButton(tela, text="Registre-se", font=("Helvetica",40)
+                                 , command=janela_registro, fg_color="#8690AF",text_color="white", width=370, height=70)
 botao2.place(x=480, y=440)
 
 # Frames
 frame_login = customtkinter.CTkFrame(master= tela, fg_color="#CFD8DC")
 frame_registro = customtkinter.CTkFrame(tela, fg_color="#CFD8DC")
+botao_login = customtkinter.CTkButton(frame_login, text="Entrar", fg_color="#8690AF", text_color="white", command=fazer_login)
+botao_registro = customtkinter.CTkButton(frame_registro, text="Criar conta", fg_color="#8690AF", text_color="white", command=criar_conta)
 
 
 #Frame/Tela de login
 texto_login = customtkinter.CTkLabel(frame_login, text="Fazer login", text_color="#607D8B", font=("Helvetica", 70))
 texto_login.place(x=510, y=180)
-email = customtkinter.CTkEntry(frame_login, placeholder_text="Digite seu usuário", text_color="black", fg_color="white", width=370, height=35, font=("Helvetica",20))
+email = customtkinter.CTkEntry(frame_login, placeholder_text="Digite seu usuário", text_color="black",
+                               fg_color="white", width=370, height=35, font=("Helvetica",20))
 email.place(x=500, y=280)
-senha = customtkinter.CTkEntry(frame_login, placeholder_text="Digite sua senha", text_color="black", fg_color="white", width=370, height=35, font=("Helvetica",20), show="*")
+senha = customtkinter.CTkEntry(frame_login, placeholder_text="Digite sua senha", text_color="black", fg_color="white", 
+                               width=370, height=35, font=("Helvetica",20), show="*")
 senha.place(x=500, y=330)
-botao_login = customtkinter.CTkButton(frame_login, text="Entrar", fg_color="#8690AF", text_color="white", command=clique)
+botao_login = customtkinter.CTkButton(frame_login, text="Entrar", fg_color="#8690AF", text_color="white", command=fazer_login)
 botao_login.place(x=730, y=400)
 botao_voltar_login = customtkinter.CTkButton(frame_login, text="Voltar",fg_color="#8690AF", command=lambda: voltar_para_inicio(frame_login))
 botao_voltar_login.place(x=500, y=400)#Botão com função de voltar pra tela inicial
@@ -68,13 +107,16 @@ botao_voltar_login.place(x=500, y=400)#Botão com função de voltar pra tela in
 #Frame/Tela de registro
 texto_registro = customtkinter.CTkLabel(frame_registro, text="Registrar-se", text_color="#607D8B", font=("Helvetica", 70))
 texto_registro.place(x=491, y=180)
-email_registro = customtkinter.CTkEntry(frame_registro, placeholder_text="Digite seu novo usuário", text_color="black", fg_color="white", width=370, height=35, font=("Helvetica",20))
+email_registro = customtkinter.CTkEntry(frame_registro, placeholder_text="Digite seu novo usuário", text_color="black", fg_color="white",
+                                        width=370, height=35, font=("Helvetica",20))
 email_registro.place(x=500, y=280)
-senha_registro = customtkinter.CTkEntry(frame_registro, placeholder_text="Digite sua senha", text_color="black", fg_color="white", width=370, height=35, font=("Helvetica",20), show="*")
+senha_registro = customtkinter.CTkEntry(frame_registro, placeholder_text="Digite sua senha", text_color="black", fg_color="white",
+                                        width=370, height=35, font=("Helvetica",20), show="*")
 senha_registro.place(x=500, y=330)
-botao_registro = customtkinter.CTkButton(frame_registro, text="Criar conta", fg_color="#8690AF", text_color="white", command=clique)
+botao_registro = customtkinter.CTkButton(frame_registro, text="Criar conta", fg_color="#8690AF", text_color="white", command=criar_conta)
 botao_registro.place(x=730, y=400)
-botao_voltar_registro = customtkinter.CTkButton(frame_registro, text="Voltar",fg_color="#8690AF", command=lambda: voltar_para_inicio(frame_registro))
+botao_voltar_registro = customtkinter.CTkButton(frame_registro, text="Voltar",fg_color="#8690AF", 
+                                                command=lambda: voltar_para_inicio(frame_registro))
 botao_voltar_registro.place(x=500, y=400)
 
 tela.mainloop()
